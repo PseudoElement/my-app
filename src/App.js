1e63ecd89a1536386876a7838a64c22e5dfcd6f2
PseudoElement <bubamainer@mail.ru>
2023-01-12 05:21:28 +0300

    useState, useRef hooks

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import Counter from "./components/counter/Counter";
 import Input from "./components/input/input";
 import "./App.css";
 import "./components/input/input.css";
-import "./components/post/post.css"
+import "./components/post/post.css";
 import Post from "./components/post/post";
 function App() {
+  let [arr, setArr] = React.useState([1, 2, 3]);
+  const arrRef = React.useRef(arr);
+  function pushArr() {
+    setArr([...arr, arr[arr.length - 1] + 1]);
+  }
   return (
     <div className="App">
-      <Counter/>
-      <Input/>
-      <Post options={{type:'block1', id: 1}}/>
-      <Post options={{type:'block2', id: 2}}/>
-      <Post options={{type:'block3', id: 3}}/>
+      <button onClick={pushArr}>FOR TEST</button>
+      <Counter />
+      <Input />
+      <Post options={{ type: "block1", id: 1 }} />
+      <Post options={{ type: "block2", id: 2 }} />
+      <Post options={{ type: "block3", id: 3 }} />
     </div>
   );
 }
