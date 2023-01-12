148f3206717924621c538fa17ce63ac75722b73c
PseudoElement <bubamainer@mail.ru>
2023-01-13 01:48:49 +0300

    useState

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import React from "react";
 import Counter from "./components/counter/Counter";
 import Input from "./components/input/input";
 import "./App.css";
-import "./components/input/input.css";
-import "./components/post/post.css"
 import Post from "./components/post/post";
 function App() {
   return (
     <div className="App">
       <Counter/>
       <Input/>
-      <Post options={{type:'block1', id: 1}}/>
-      <Post options={{type:'block2', id: 2}}/>
-      <Post options={{type:'block3', id: 3}}/>
+      <Post options={{type:'block1', id: 1, isDisabled: true}}/>
+      <Post options={{type:'block2', id: 2, isDisabled: false}}/>
+      <Post options={{type:'block3', id: 3, isDisabled: true}}/>
     </div>
   );
 }
--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -1,21 +1,38 @@
 import React, { useState } from "react";
+import "./post.css";
 function Post(props) {
-  let { id, type } = props.options;
-  let [postContents] = useState([
-    { text: "Some text 1" },
-    { text: "text 2" },
-    { text: "text 3333" },
+  let { id, isDisabled } = props.options;
+  let [postContents, setPosts] = useState([
+    { text: "Some text 1", number: 1 },
+    { text: "text 2", number: 2 },
+    { text: "text 3333", number: 3 },
   ]);
+  const [text, setText] = useState("Type here...");
+  function addPost() {
+    const newPost = { id: Date.now(), text };
+    postContents = [...postContents, newPost];////с postContents.push(newPost) не работает 
+    setPosts(postContents);
+    setText('')
+  }
   return (
     <div className="post">
-        {postContents.map(string=>{
-            return <div className="post_content">{string.text}</div>
-        })}
+      {postContents.map((post, index) => {
+        return <div className="post_content">{index + 1}. {post.text}</div>;
+      })}
+      <input
+        onChange={(e) => setText(e.target.value)}
+        className="post_input"
+        type="text"
+        value={text}
+      />
       <div className="post_btns">
-        <button style={{ background: id === 2 ? "green" : "red" }}>
+        <button onClick={addPost}>Add</button>
+        <button
+          disabled={isDisabled ? true : false}
+          style={{ background: id === 2 ? "green" : "red" }}
+        >
           Delete
         </button>
-        {type === "block1" ? <button>Submit</button> : null}
       </div>
     </div>
   );
